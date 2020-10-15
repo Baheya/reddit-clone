ec071200114a0143c48c8689caf4c45d56c77117
Baheya <khalifa.baheya@gmail.com>
2020-10-15 11:04:44 +0400

    added heroku postgresql env var

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,7 @@ import { createUpvoteLoader } from './utils/createUpvoteLoader';
 const main = async () => {
   await createConnection({
     type: 'postgres',
-    url: process.env.DATABASE_URL,
+    url:  process.env.HEROKU_POSTGRESQL_ORANGE_URL || process.env.DATABASE_URL,
     logging: true,
     // synchronize: true,
     migrations: [path.join(__dirname, './migrations/*')],
