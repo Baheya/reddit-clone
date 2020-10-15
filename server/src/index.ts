410eaae23e3a24184b36df66448de4de63c6093d
Baheya <khalifa.baheya@gmail.com>
2020-10-15 20:06:06 +0400

    added procfile to deploy both frontend and backend to heroku

--- server/src/index.ts
+++ server/src/index.ts
@@ -83,7 +83,7 @@ const main = async () => {
   });
 
   if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "../web/build")));
   }
   
   app.listen(parseInt(process.env.PORT), () => {
