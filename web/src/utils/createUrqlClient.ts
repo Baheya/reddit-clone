e910fb9aa0b32c3af6e26d973a5116f677a85a56
Baheya <khalifa.baheya@gmail.com>
2020-10-18 16:38:44 +0400

    updated environment vars

--- web/src/utils/createUrqlClient.ts
+++ web/src/utils/createUrqlClient.ts
@@ -84,7 +84,7 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
     cookie = ctx?.req?.headers?.cookie;
   }
   return {
-    url: 'http://localhost:8080/graphql',
+    url: process.env.NEXT_PUBLIC_API_URL,
     fetchOptions: {
       credentials: 'include' as const,
       headers: cookie ? { cookie } : undefined,
