22f86efe8c567b85ad92628af46ed5b5dc543ebc
Baheya <khalifa.baheya@gmail.com>
2020-10-20 19:51:54 +0400

    added domain to cookie configuration, supposed to work now that frontend and backend are deployed on the same custom domain

--- server/src/index.ts
+++ server/src/index.ts
@@ -53,6 +53,7 @@ const main = async () => {
         httpOnly: true,
         sameSite: 'lax', // csrf
         secure: __prod__, // cookie only works in https
+        domain: __prod__ ? ".baheya.dev" : undefined,
       },
       saveUninitialized: false,
       secret: process.env.SESSION_SECRET,
