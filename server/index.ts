fa79b9fe56d759eb96e47b69049439bcd60dbfe0
Baheya <khalifa.baheya@gmail.com>
2020-10-27 07:47:51 +0400

    updated cookie options

--- server/index.ts
+++ server/index.ts
@@ -53,7 +53,7 @@ const main = async () => {
         httpOnly: true,
         sameSite: 'lax', // csrf
         secure: __prod__, // cookie only works in https
-        domain: ".baheya.dev",
+        domain: __prod__ ? ".baheya.dev" : undefined,
       },
       saveUninitialized: false,
       secret: process.env.SESSION_SECRET,
